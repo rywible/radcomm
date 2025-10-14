2128cb4fbf30ea34f05cf5092499e34f610ff8da
Ryan Wible <ryanwiblecoding@gmail.com>
2025-10-13 21:06:51 -0600

    storefront landing page first pass

--- storefront/astro.config.mjs
+++ storefront/astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    // @ts-expect-error
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@core": new URL("../core/src", import.meta.url).pathname,
+        "@storefront": new URL("./src", import.meta.url).pathname,
+        "@ui": new URL("./src/components/ui", import.meta.url).pathname,
+      },
+    },
+    server: {
+      fs: {
+        // allow importing from monorepo root
+        allow: [new URL("../..", import.meta.url).pathname],
+      },
+    },
+  },
+});
